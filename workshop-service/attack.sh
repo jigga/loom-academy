33f2f7190543aa9f27f6cc5bb1a97d7b9518634e
jigga <jigga@jigga.pl>
2019-09-17 19:51:20 +0200

    Reverted timeout to 100.

--- workshop-service/attack.sh
+++ workshop-service/attack.sh
@@ -1,6 +1,6 @@
 case  $1 in
   fibers)
-    echo "GET http://localhost:8080/loom/test" | vegeta attack -duration=120s -timeout=300s -rate=400 | tee  $1.bin | vegeta report && cat $1.bin | vegeta plot > $1.html
+    echo "GET http://localhost:8080/loom/test" | vegeta attack -duration=120s -timeout=300s -rate=100 | tee  $1.bin | vegeta report && cat $1.bin | vegeta plot > $1.html
       ;;
   threads)
     echo "GET http://localhost:8081/loom/test" | vegeta attack -duration=120s -timeout=300s -rate=100 | tee  $1.bin | vegeta report && cat $1.bin | vegeta plot > $1.html
